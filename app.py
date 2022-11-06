27eac37c6f32472832f4d0aaabe5c1762317dee6
Takuya <kyuzaemon.miza@gmail.com>
2022-11-06 15:04:04 +0900

    make better

--- app.py
+++ app.py
@@ -7,26 +7,30 @@ app.config['SECRET_KEY'] = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///zangyo.sqlite'
 
 db = SQLAlchemy(app)
-class Task(db.Model):
+class Overtime(db.Model):
 
-    __tablename__ = "tasks"
+    __tablename__ = "overtimes"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    text = db.Column(db.Text())
     status = db.Column(db.Integer)
+    date = db.Column(db.String())
+    time = db.Column(db.Float())
+    total_time = db.Column(db.Float())
 
 db.create_all()
 
 @app.route('/')
 def index():
-    tasks = Task.query.all()
-    return render_template("index.html", tasks = tasks)
+    overtimes = Overtime.query.all()
+    return render_template("index.html", overtimes = overtimes)
 
 @app.route('/new', methods=["POST"])
 def new():
-    task = Task()
-    task.text = request.form["new_text"]
-    task.status = 0
-    db.session.add(task)
+    overtime = Overtime()
+    overtime.date = request.form["new_date"]
+    overtime.time = request.form["new_text"]
+    overtime.total_time = 1 # 仮で1
+    overtime.status = 0
+    db.session.add(overtime)
     db.session.commit()
     return redirect(url_for('index'))
 
