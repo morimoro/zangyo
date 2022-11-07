e437f3a30b13b1f10dc87e853dab776a1469e51f
Takuya <kyuzaemon.miza@gmail.com>
2022-11-07 23:06:09 +0900

    2022/11/7 /createを作成

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 # from sqlalchemy import desc #降順に並べ替えの時に必要
 import os
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
@@ -17,6 +18,7 @@ class Overtime(db.Model):
     date = db.Column(db.String())
     time = db.Column(db.Float())
     total_time = db.Column(db.Float())
+    time_36 = db.Column(db.Float())
 
 with app.app_context():
     db.create_all()
@@ -32,7 +34,20 @@ def index():
 def delete():
     Overtime.query.delete() # Overtimeデータベース削除
     db.session.commit()
-    return render_template("index.html")
+    return redirect(url_for('index'))
+
+@app.route('/create', methods=["GET"])
+def create():
+    print('create')
+    test = [Overtime(status = 0, time = 1, date = datetime.date(2022, 4, i+1), total_time = 0 ,time_36 = 0) for i in range(30)] 
+    db.session.add_all(test)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update', methods=["GET"])
+def update():
+    print('update')
+    return redirect(url_for('index'))
 
 @app.route('/new', methods=["POST"])
 def new():
@@ -41,11 +56,11 @@ def new():
     overtime.time = float(request.form["new_text"])
     totals = db.session.query(Overtime.time).all()
     sum = 0
-    print(totals)
     for total in totals:
         sum = sum + total[0]
     overtime.total_time = sum + overtime.time
     overtime.status = 0
+    overtime.time_36 = 1
     db.session.add(overtime)
     db.session.commit()
     return redirect(url_for('index'))
