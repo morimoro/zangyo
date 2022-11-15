ef880e2eca7dcd9c9346524f260346d997ad85ac
Takuya <kyuzaemon.miza@gmail.com>
2022-11-15 21:47:33 +0900

    2022/11/15 ほぼ完成

--- app.py
+++ app.py
@@ -29,22 +29,48 @@ class Overtime(db.Model):
     time_36 = db.Column(db.Float())
     estimated_time = db.Column(db.Float())
 
+with app.app_context():
+        db.create_all()
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == 'GET':
-        with app.app_context():
-            db.create_all()
-        overtimes = Overtime.query.order_by(Overtime.date).all() # 日付を昇順に並べ替え
+
+         #データベースのデータ数
+        date_count = Overtime.query.count()
+        print(date_count)
+
+        #月間予定残業に最終日の理想残業時間を代入
+        #データがなければ初期値として0を代入
+        try:
+            overtime = db.session.query(Overtime).filter(Overtime.id==date_count).first()
+            scheduled_overtime = overtime.estimated_time
+        except:
+            scheduled_overtime = 0
+        
+        #前月最終日36残業時間に36残業-残業時間を代入
+        #データベースがなければ初期値として0を代入
+        try:
+            overtime = db.session.query(Overtime).filter(Overtime.id==1).first()
+            last_month_36_overtime = overtime.time_36 - overtime.time
+        except:
+            last_month_36_overtime = 0
+        
+        #月間稼働日に初期値として0を代入
+        working_days = 0
+
+        #日付を昇順に並べ替え
+        overtimes = Overtime.query.order_by(Overtime.date).all() 
         db.session.commit()
-        scheduled_overtime = 0 #月間予定残業に初期値として0を代入
-        last_month_36_overtime = 0  #前月最終日36残業時間に初期値として0を代入
-        working_days = 0 #月間稼働日に初期値として0を代入
+
         return render_template("index.html",
         overtimes = overtimes, scheduled_overtime=scheduled_overtime, last_month_36_overtime=last_month_36_overtime, working_days=working_days)
 
     else:
         print('update')
-        date_count = Overtime.query.count() # データベースのデータ数
+
+        #データベースのデータ数
+        date_count = Overtime.query.count()
         print(date_count)
 
         #月間予定残業を取得、未入力なら0を代入
@@ -59,7 +85,8 @@ def index():
             sum_36 = last_month_36_overtime 
         except:
             last_month_36_overtime = 0
-            sum_36 = db.session.query(Overtime).filter(Overtime.id==1).first().time_36 - db.session.query(Overtime).filter(Overtime.id==1).first().time
+            overtime = db.session.query(Overtime).filter(Overtime.id==1).first()
+            sum_36 = overtime.time_36 - overtime.time
 
         # データベースの回数だけ繰り返し、値を取得して月間残業、36残業を計算
         sum = 0
@@ -76,15 +103,14 @@ def index():
                 sum_36 = 0
             else:
                 sum_36 = overtime.time_36
-            db.session.commit()
-        with app.app_context():
-            db.create_all()
-        overtimes = Overtime.query.order_by(Overtime.date).all() # 日付を昇順に並べ替え
         db.session.commit()
 
+        #出勤日をカウントして月間稼働日数とする
         working_days = db.session.query(Overtime).filter(Overtime.status==1).count()
+        #毎日の平均残業時間 = 月間予定残業 / 月間稼働日数
         ave_estimated_time = scheduled_overtime / working_days
         sum_estimated_time = 0
+        #出勤日の場合、理想残業時間に毎日の平均残業時間を加算
         for i in range(date_count):
             overtime = db.session.query(Overtime).filter(Overtime.id==(i+1)).first()
             if overtime.status == 1:
@@ -92,6 +118,10 @@ def index():
                 sum_estimated_time = overtime.estimated_time
             else:
                 overtime.estimated_time = sum_estimated_time
+        db.session.commit()
+
+        #日付を昇順に並べ替え
+        overtimes = Overtime.query.order_by(Overtime.date).all()
         db.session.commit()
 
         return render_template("index.html",
@@ -138,22 +168,22 @@ def create():
 
         return redirect(url_for('index'))
 
-@app.route('/new', methods=["POST"])
-def new():
-    overtime = Overtime()
-    date = datetime.datetime.strptime(request.form["new_date"], '%Y-%m-%d') #型変換
-    overtime.date = datetime.date(date.year, date.month, date.day) #年月日だけに変換
-    overtime.weekday = date.weekday()
-    overtime.time = float(request.form["new_text"])
-    totals = db.session.query(Overtime.time).all()
-    sum = 0
-    for total in totals:
-        sum = sum + total[0]
-    overtime.total_time = sum + overtime.time
-    overtime.status = 0
-    overtime.time_36 = 1
-    db.session.add(overtime)
-    db.session.commit()
-    return redirect(url_for('index'))
+# @app.route('/new', methods=["POST"])
+# def new():
+#     overtime = Overtime()
+#     date = datetime.datetime.strptime(request.form["new_date"], '%Y-%m-%d') #型変換
+#     overtime.date = datetime.date(date.year, date.month, date.day) #年月日だけに変換
+#     overtime.weekday = date.weekday()
+#     overtime.time = float(request.form["new_text"])
+#     totals = db.session.query(Overtime.time).all()
+#     sum = 0
+#     for total in totals:
+#         sum = sum + total[0]
+#     overtime.total_time = sum + overtime.time
+#     overtime.status = 0
+#     overtime.time_36 = 1
+#     db.session.add(overtime)
+#     db.session.commit()
+#     return redirect(url_for('index'))
 
 app.run(debug=True, host=os.getenv('APP_ADDRESS', 'localhost'), port=8001)
